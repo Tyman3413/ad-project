517a46ef6d269ab6f3dee44a4eef5d428fec1a9d
Tyman3413 <tyman3413@gmail.com>
2021-12-03 10:38:35 +0300

    3.4

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -4,35 +4,34 @@
             <v-flex xs12 sm8 md8>
                 <v-card class="elevetion-12">
                     <v-toolbar dark color="primary">
-                        <v-toolbar-title>Login
-                        </v-toolbar-title>
+                        <v-toolbar-title>Login</v-toolbar-title>
                     </v-toolbar>
-                        <v-card-text>
-                            <v-form 
-                                v-model="valid" 
-                                ref="form"
-                                validation>
-                                    <v-text-field
-                                        prepend-icon="mdi-account" 
-                                        name="email" 
-                                        label="Email" 
-                                        type="email" 
-                                        v-model="email"
-                                        :rules="emailRules" >
-                                    </v-text-field>
-                                    <v-text-field  
-                                        prepend-icon="mdi-lock" 
-                                        name="password" 
-                                        label="Password" 
-                                        type="password" 
-                                        v-model="password" 
-                                        :rules="passwordRules">
-                                    </v-text-field>
-                            </v-form> 
-                        </v-card-text>
+                    <v-card-text>
+                        <v-form v-model="valid" ref="form" validation>
+                            <v-text-field
+                                prepend-icon="mdi-account" 
+                                name="email" 
+                                label="Email" 
+                                type="email" 
+                                v-model="email"
+                                :rules="emailRules" >
+                            </v-text-field>
+                            <v-text-field  
+                                prepend-icon="mdi-lock" 
+                                name="password" 
+                                label="Password" 
+                                type="password" 
+                                v-model="password"
+                                :rules="passwordRules" >
+                            </v-text-field>
+                        </v-form> 
+                    </v-card-text>
                     <v-card-actions>
                         <v-spacer></v-spacer>
-                        <v-btn color="primary">
+                        <v-btn 
+                            color="primary"
+                            @click="onSubmit"
+                            :disabled="!valid">
                             Login
                         </v-btn>
                     </v-card-actions>	
@@ -43,7 +42,6 @@
 </template>
 
 <script>
-    
 export default {
     data () { 
         return {
@@ -51,14 +49,26 @@ export default {
             password: "",
             valid: false,
             emailRules: [
-                v => !!v || 'E-mail is required',
-                    v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+            v => !!v || 'E-mail is required',
+            v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
             ],
             passwordRules: [
-                v => !!v || 'Name is required',
-                    v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters',
+            v => !!v || 'Password is required',
+            v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters'
             ]
         } 	
+    },
+    
+    methods: {
+        onSubmit(){
+            if (this.$refs.form.validate()){
+                const user = {
+                    email: this.email,
+                    password: this.password
+                }
+                console.log(user)
+            }
+        }
     }
 } 
 </script>
