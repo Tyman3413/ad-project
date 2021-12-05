82cd18cd739a177138bb063a0e87ba593aa6b333
Tyman3413 <tyman3413@gmail.com>
2021-12-05 15:44:05 +0300

    4.3

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -7,7 +7,7 @@
                         <v-toolbar-title>Sign Up</v-toolbar-title>
                     </v-toolbar>
                     <v-card-text>
-                        <v-form v-model="valid" ref="form" validation>
+                        <v-form v-model="valid" ref="form" lazy-validation>
                             <v-text-field
                                 prepend-icon="mdi-account" 
                                 name="email" 
