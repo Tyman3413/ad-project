e26b30cfd92db2edd4f274af614d527e7cecf561
Tyman3413 <tyman3413@gmail.com>
2022-01-23 18:46:02 +0300

    12.3

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router/index'
 import store from './store'
+import fb from 'firebase'
 
 Vue.use(Router)
 Vue.config.productionTip = false
@@ -12,5 +13,19 @@ new Vue({
   vuetify,
   render: h => h(App),
   router:router,
-  store
+  store,
+  created(){
+  const firebaseConfig = {
+    apiKey: "AIzaSyD-36U12uImdv5Kkp2_EY6HFBR9b9R8Xxw",
+    authDomain: "ad-pro-19577.firebaseapp.com",
+    projectId: "ad-pro-19577",
+    storageBucket: "ad-pro-19577.appspot.com",
+    messagingSenderId: "127600040099",
+    appId: "1:127600040099:web:7eba278180a4b18dc64716",
+    measurementId: "G-GXVLCVZ3TB"
+  };
+  // Initialize Firebase
+  fb.initializeApp(firebaseConfig);
+  fb.analytics();
+}
 }).$mount('#app')
